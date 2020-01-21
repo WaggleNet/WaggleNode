6ac5c4751451fe1ca40450d5d3bdeccc885d0ad4
Jimmy He <jimmy.xrail@me.com>
2020-01-21 03:47:47 -0600

    Fix pinout for voltage sensing pins

--- src/pinout.h
+++ src/pinout.h
@@ -3,8 +3,8 @@
 #define CHRG_CHARGING 7
 #define CHRG_STANDBY 8
 
-#define BATT_SENS A6
-#define SOLAR_SENS A7
+#define SOLAR_SENS A6
+#define BATT_SENS A7
 
 #define LED_BLUE 6
 #define LED_ORANGE 4
